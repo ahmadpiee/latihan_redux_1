73e23239eb73886de415902e6b462a18e707c3e7
ahmadpiee <ahmadpiee69@gmail.com>
2021-09-15 00:24:03 +0700

    fixed

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
 import { persistedReducer } from "./reducers/index";
+import { composeWithDevTools } from "redux-devtools-extension";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 
 const bindMiddleware = () => {
     if (process.env.NODE_ENV !== "production") {
-        const { composeWithDevTools } = require("redux-devtools-extension");
         return composeWithDevTools(applyMiddleware(thunk, logger));
     }
     return applyMiddleware(thunk);
