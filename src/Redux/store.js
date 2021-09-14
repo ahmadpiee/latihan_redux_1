288d02c739dd97b3453d917d126b858a1292083b
ahmadpiee <ahmadpiee69@gmail.com>
2021-09-15 00:17:00 +0700

    improve a bit store.js

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,13 +1,17 @@
 import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
 import { persistedReducer } from "./reducers/index";
-import { composeWithDevTools } from "redux-devtools-extension";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 
-export const store = createStore(
-    persistedReducer,
-    composeWithDevTools(applyMiddleware(thunk, logger))
-);
+const bindMiddleware = () => {
+    if (process.env.NODE_ENV !== "production") {
+        const { composeWithDevTools } = require("redux-devtools-extension");
+        return composeWithDevTools(applyMiddleware(thunk, logger));
+    }
+    return applyMiddleware(thunk);
+};
+
+export const store = createStore(persistedReducer, bindMiddleware());
 
 export const persistor = persistStore(store);
